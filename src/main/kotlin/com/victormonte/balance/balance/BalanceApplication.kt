03525c48559fa28c6a1aa1c7677ee038bcf4d0e9
Victor Monte <victorbcm@hotmail.com>
2018-10-04 17:24:26 -0300

    Initial commit

---
+++ src/main/kotlin/com/victormonte/balance/balance/BalanceApplication.kt
@@ -0,0 +1,11 @@
+package com.victormonte.balance.balance
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BalanceApplication
+
+fun main(args: Array<String>) {
+    runApplication<BalanceApplication>(*args)
+}
